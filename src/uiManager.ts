972416a8b74443e03726d0ba64212bfe57383501
Alexandre Ait Ettajer <alexandre.aitettajer@gmail.com>
2025-03-03 19:14:16 -0500

    Fixed indentation space

--- src/uiManager.ts
+++ src/uiManager.ts
@@ -6,55 +6,55 @@ import { InventoryPanel } from "./inventoryPanel";
 import { GameState } from "./gamestate";
 
 export class UIManager {
-    logPanel: LogPanel
-    playerPanel: PlayerPanel
-    inventoryPanel: InventoryPanel
-    gameState: GameState | null = null;
+  logPanel: LogPanel
+  playerPanel: PlayerPanel
+  inventoryPanel: InventoryPanel
+  gameState: GameState | null = null;
 
-    focusObjectQueue: Array<UIComponent> = new Array
-    get focused (): boolean {
-        return this.focusObjectQueue.length > 0; 
-    }
-    get focusedObject (): UIComponent | null {
-        return this.focused ? this.focusObjectQueue[this.focusObjectQueue.length - 1] : null;
-    }
+  focusObjectQueue: Array<UIComponent> = new Array
+  get focused (): boolean {
+    return this.focusObjectQueue.length > 0; 
+  }
+  get focusedObject (): UIComponent | null {
+    return this.focused ? this.focusObjectQueue[this.focusObjectQueue.length - 1] : null;
+  }
 
-    constructor() {
-        this.logPanel = new LogPanel(new Position(0, 40, 80, 4));
-        this.playerPanel = new PlayerPanel(new Position(80, 10, 40, 10));
-        this.inventoryPanel = new InventoryPanel(new Position(10, 5, 60, 30));
-    }
+  constructor() {
+    this.logPanel = new LogPanel(new Position(0, 40, 80, 4));
+    this.playerPanel = new PlayerPanel(new Position(80, 10, 40, 10));
+    this.inventoryPanel = new InventoryPanel(new Position(10, 5, 60, 30));
+  }
 
-    async updateContent() {
-        await this.focusedObject.updateContent();
-    }
+  async updateContent() {
+    await this.focusedObject.updateContent();
+  }
 
-    refreshVisual() {
-        this.logPanel.refreshVisuals();
-        this.playerPanel.refreshVisuals();
-        if (this.focused) {
-            this.focusedObject.refreshVisuals();
-        }
+  refreshVisual() {
+    this.logPanel.refreshVisuals();
+    this.playerPanel.refreshVisuals();
+    if (this.focused) {
+      this.focusedObject.refreshVisuals();
     }
+  }
 
-    openInventory (): void {
-        this.focusObjectQueue.push(this.inventoryPanel);
-    }
+  openInventory (): void {
+    this.focusObjectQueue.push(this.inventoryPanel);
+  }
 
-    exitCurrentFocus (): void {
-        this.focusObjectQueue.pop();
-        if (!this.focusObjectQueue.length && this.gameState) {
-            this.gameState.fullRefresh();
-        }
+  exitCurrentFocus (): void {
+    this.focusObjectQueue.pop();
+    if (!this.focusObjectQueue.length && this.gameState) {
+      this.gameState.fullRefresh();
     }
+  }
 }
 
 
 let uiManager = new UIManager;
 export function getUIManager() {
-    return uiManager;
+  return uiManager;
 }
 
 export function logMessage(text: string) {
-    uiManager.logPanel.addLogMessage(text);
+  uiManager.logPanel.addLogMessage(text);
 }
