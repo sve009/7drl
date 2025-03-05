d08522bab4b2e497ef4bc5ef6b0adebca981c4e4
Alexandre Ait Ettajer <alexandre.aitettajer@gmail.com>
2025-03-04 20:50:50 -0500

    Add seed log

--- src/playerPanel.ts
+++ src/playerPanel.ts
@@ -1,9 +1,15 @@
+import { RNG } from "rot-js";
 import { UIComponent } from "./gameObject"
 import { Player } from "./player";
 import { Position, TextDrawable } from "./renderer"
 
-export class PlayerPanel extends UIComponent{
+export class PlayerPanel extends UIComponent {
   player: Player
+
+  constructor (boundary: Position) {
+    super(boundary);
+    this.layer.bg = "#9c2f6c";
+  }
 
   refreshVisuals () {
     super.refreshVisuals();
@@ -12,15 +18,19 @@ export class PlayerPanel extends UIComponent{
     `Health: ${this.player.health}/${this.player.maxHealth}\n` +
     `Distance Traveled: ${this.player.distanceTraveled}\n` +
     `Position: [${this.player.position.x}, ${this.player.position.y}]\n` +
-    `Level: ${this.player.dungeonLevel}\n`;
+    `Level: ${this.player.dungeonLevel}\n` +
+    `Seed: ${RNG.getSeed()}`;
 
     playerStats = ` %b{${this.layer.bg}}${playerStats}`;
 
     this.layer.addDrawable(new TextDrawable(this.boundaries.getStartX(), this.boundaries.getStartY(), playerStats));
   }
-  
-  constructor (boundary: Position) {
-    super(boundary);
-    this.layer.bg = "#9c2f6c";
+
+  private getHealth(): string {
+    return `Health: ${this.player.health}/${this.player.maxHealth}`;
+  }
+
+  private get distTraveled () {
+
   }
 }
