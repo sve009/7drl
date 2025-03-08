fb2472226c6be198de7366551b4600699ff76b98
Alexandre Ait Ettajer <alexandre.aitettajer@gmail.com>
2025-03-08 07:57:04 -0500

    Started on descriptions

--- src/descriptionCatalogue.ts
+++ src/descriptionCatalogue.ts
@@ -1,129 +1,129 @@
 export function descriptionCatalogue (key: string) {
   switch (key) {
     case "#":
-      break;
+      return "Wall: Just regular old stone";
     case ".":
-      break;
+      return "Ground: Unimpressive if you ask me"
     case "+":
-      break;
+      return "Door: What could be on the other side?"
     case "grass":
-      break;
+      return "Grass: The blades sway in the soft wind."
     case "bush":
-      break;
+      return "Bush: Is it rustling slightly, I hope there isn't a goblin hiding in there.";
     case "shrub":
-      break;
+      return "Shrub: We are the nights that say NEE!";
     case "altar":
-      break;
+      return "Altar: The priest talks about artifacts and "
     case "water":
-      break;
+      return "Deep Water: A little too deep for someone who doesn't know how to swim to enter."
     case "swater":
-      break;
+      return "Shallow Water: The water only reaches to your knees."
     case "leaf":
-      break;
+      return "Leaves: The birds tweet from the branches."
     case "hshelf":
-      break;
+      return "Shelf: The shelves seem older than the priest, and word is they're at least 100 years old."
     case "vshelf":
-      break;
+      return "Shelf: Where do these books come from, they're all written in languages unknown to you."
     case ">":
-      break;
+      return "Descending Stairs: What is hidden in the depths of the dungeon?"
     case "<":
-      break;
+      return "Ascending Stairs: Brings you back to safety!"
     case "carpet":
-      break;
+      return "Carpet: Red interesting choice."
     case "woodf":
-      break;
+      return "Wood Floor: "
     case "path":
-      break;
+      return "Path: Leads you around the monastery."
     case "statue1":
-      break;
+      return "Statue: "
     case "statue2":
-      break;
+      return "Statue: "
     case "lilypad":
-      break;
+      return "Lilypad: "
     case "reed":
-      break;
+      return "Reed: "
     case "ruin1":
-      break;
+      return "Ruin: "
     case "ruin2":
-      break;
+      return "Ruin: "
     case "ruin3":
-      break;
+      return "Ruin: "
     case "ruin4":
-      break;
+      return "Ruin: "
     case "ruin5":
-      break;
+      return "Ruin: "
     case "ruin6":
-      break;
+      return "Ruin: "
     case "artfloor":
-      break;
+      return "???"
     case "artpill":
-      break;
+      return "???";
     case "tele1":
-      break;
+      return "Teleporter: "
     case "tele2":
-      break;
+      return "Teleporter: "
     case "tele3":
-      break;
+      return "Teleporter: "
     case "tele4":
-      break;
+      return "Teleporter: "
     case "shopkeeper":
-      break;
+      return "Shopkeeper: "
     case "priest":
-      break;
+      return "Priest: "
     case "alchemist":
-      break;
+      return "Alchemist: "
     case "blacksmith":
-      break;
+      return "Blacksmith: "
     case "librarian":
-      break;
+      return "Librarian: "
     case "bat":
-      break;
+      return "Bat: "
     case "jackal":
-      break;
+      return "Jackal: "
     case "goblin":
-      break;
+      return "Goblin: "
     case "goblinarcher":
-      break;
+      return "Goblin Archer: "
     case "wolf":
-      break;
+      return "Wolf: "
     case "potion of confusion":
-      break;
+      return "Potion of confusion: "
     case "potion of healing":
-      break;
+      return "Potion of Healing: "
     case "potion of regeneration":
-      break;
+      return "Potion of Regeneration: "
     case "potion of invisibility":
-      break;
+      return "Potion of invisibility: "
     case "unstable potion":
-      break;
+      return "Unstable Potion: "
     case "potion of might":
-      break;
+      return "Potion of Might: "
     case "scroll of teleportation":
-      break;
+      return "Scroll of Teleportation: "
     case "scroll of omniscience":
-      break;
+      return "Scroll of Omniscience: "
     case "scroll of poison":
-      break;
+      return "Scroll of Poison: "
     case "dagger":
-      break;
+      return "Dagger: "
     case "spear":
-      break;
+      return "Spear: "
     case "sword":
-      break;
+      return "Sword: "
     case "axe":
-      break;
+      return "Axe: "
     case "glaive":
-      break;
+      return "Glaive: "
     case "leather armor":
-      break;
+      return "Leather Armor: "
     case "chain mail":
-      break;
+      return "Chain mail: "
     case "scale mail":
-      break;
+      return "Scale Mail: "
     case "plate armor":
-      break;
+      return "Plate Armor: "
     case "player":
-      break;
+      return "Player: That's you!"
     default:
       throw new Error("MESSAGE ID MISSING");
   }
